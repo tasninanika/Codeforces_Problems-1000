bd1b9d56d78e6bdba83f21bf93f156f30661fccc
anikatasnin <jarintasnin09@gmail.com>
2025-01-04 15:45:54 +0600

    Added Young Physicist

---
+++ Young Physicist/youngphysicist.cpp
@@ -0,0 +1,26 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+    int n, sum1 = 0, sum2 = 0, sum3 = 0;
+    cin >> n;
+
+    int a[100][100];
+    for(int i = 0; i < n; i++){
+        for(int j = 0; j < 3; j++){
+            cin >> a[i][j];
+        }
+        sum1 += a[i][0];
+        sum2 += a[i][1];
+        sum3 += a[i][2];
+    }
+    if(sum1 == 0 && sum2 == 0 && sum3 == 0){
+        cout << "YES" << endl;
+    }
+    else{
+        cout << "NO" << endl;
+    }
+
+
+    return 0;
+}
