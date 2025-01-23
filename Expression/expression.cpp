1e60111135f9976e09092ddd08f6dc99aa66b2af
anikatasnin <jarintasnin09@gmail.com>
2025-01-23 22:16:03 +0600

    Added Expression

--- Expression/expression.cpp
+++ Expression/expression.cpp
@@ -14,19 +14,19 @@ int main(){
     r5 = a * (b + c);
     r6 = a * b * c;
 
-    if(r1>r2 && r1>r3 && r1>r4 && r1>r5 && r1>r6){
+    if(r1>=r2 && r1>=r3 && r1>=r4 && r1>=r5 && r1>=r6){
         cout << r1 << endl;
     }
-    else if(r2>r1 && r2>r3 && r2>r4 && r2>r5 && r2>r6){
+    else if(r2>=r1 && r2>=r3 && r2>=r4 && r2>=r5 && r2>=r6){
         cout << r2 << endl;
     }
-    else if(r3>r1 && r3>r2 && r3>r4 && r3>r5 && r3>r6){
+    else if(r3>=r1 && r3>=r2 && r3>=r4 && r3>=r5 && r3>=r6){
         cout << r3 << endl;
     }
-    else if(r4>r1 && r4>r2 && r4>r3 && r4>r5 && r4>r6){
+    else if(r4>=r1 && r4>=r2 && r4>=r3 && r4>=r5 && r4>=r6){
         cout << r4 << endl;
     }
-    else if(r5>r1 && r5>r2 && r5>r3 && r5>r4 && r5>r6){
+    else if(r5>=r1 && r5>=r2 && r5>=r3 && r5>=r4 && r5>=r6){
         cout << r5 << endl;
     }
     else{
