2edc4458d6ce451c1a27ef10b52ef38fc409056f
anikatasnin <jarintasnin09@gmail.com>
2025-01-30 09:58:27 +0600

    Added cAPS lOCK

--- cAPS lOCK/capslock.cpp
+++ cAPS lOCK/capslock.cpp
@@ -5,13 +5,20 @@ int main(){
     string s;
     cin >> s;
 
-    int c = 0;
+    int c1 = 0, c2 = 0;
 
     for(int i = 1; i < s.size(); i++){
         if(islower(s[i])){
             c++;
         }
     }
+
+    for(int i = 0; i < s.size(); i++){
+        if(isupper(s[i])){
+            d++;
+        }
+    }
+
     if(islower(s[0]) && c == 0){
         s[0] = toupper(s[0]);
         for(int i = 1; i < s.size(); i++){
