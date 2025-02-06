b7d4daa9f437a6c8ddc4c53176a2f219ec298482
anikatasnin <jarintasnin09@gmail.com>
2025-02-06 18:59:41 +0600

    Added Dragons

--- Dragons/dragons.cpp
+++ Dragons/dragons.cpp
@@ -1,13 +1,22 @@
 #include<iostream>
+#include<bits/stdc++.h>
 using namespace std;
 
 int main(){
     int s, n, c = 0;
     cin >> s >> n;
 
-    int x[n], y[n];
+    pair <int,int> a[1000];
     for(int i = 0; i < n; i++){
-        cin >> x[i] >> y[i];
+        cin >> a[i].first >> a[i].second;
+    }
+
+    for(int i = 0; i < n; i++){
+        for(int j = 0; j < n - i - 1; j++){
+            if(a[j]>a[j+1]){
+                swap(a[j],a[j+1]);
+            }
+        }
     }
 
     for(int i = 0; i < n; i++){
