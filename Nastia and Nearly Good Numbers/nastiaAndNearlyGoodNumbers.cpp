577e401d6224155ed13926dcff21ce96c82d431f
anikatasnin <jarintasnin09@gmail.com>
2025-05-19 14:22:22 +0600

    added Nastia and Nearly Good Numbers

--- Nastia and Nearly Good Numbers/nastiaAndNearlyGoodNumbers.cpp
+++ Nastia and Nearly Good Numbers/nastiaAndNearlyGoodNumbers.cpp
@@ -12,6 +12,10 @@ int main(){
         if(b == 1){
             cout << "NO" << endl;
         }
+        else{
+            cout << "YES" << endl;
+            long long sum = (a * b * 2);
+        }
     }
 
 
