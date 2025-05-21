f6344a5f993939c47f2d5dcc6dff23fb2ab33b77
anikatasnin <jarintasnin09@gmail.com>
2025-05-21 15:35:01 +0600

    added Good Prefixes

--- Good Prefixes/goodPrefixes.cpp
+++ Good Prefixes/goodPrefixes.cpp
@@ -1,1 +1,31 @@
+#include<iostream>
+using namespace std;
+
+int main() {
+    int t;
+    cin >> t;
+
+    while (t--) {
+        int n;
+        cin >> n;
+
+        long long sum = 0, maxVal = -1, c = 0;
+
+        for (int i = 0; i < n; i++) {
+            long long x;
+            cin >> x;
+
+            maxVal = max(maxVal, x);
+            sum += x;
+
+            if (sum == 2 * maxVal) {
+                c++;
+            }
+        }
+
+        cout << c << endl;;
+    }
+
+    return 0;
+}
 
