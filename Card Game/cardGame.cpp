4d931ea6df53bdfa9908b933436f4eb52e8efe93
anikatasnin <jarintasnin09@gmail.com>
2025-05-21 14:58:41 +0600

    added Card Game

--- Card Game/cardGame.cpp
+++ Card Game/cardGame.cpp
@@ -1,4 +1,4 @@
-#include <iostream>
+#include<iostream>
 using namespace std;
 
 int main() {
@@ -9,11 +9,8 @@ int main() {
         int a, b, c, d;
         cin >> a >> b >> c >> d;
 
-        int res = 0;
-
-        if ((a > c && b > d) || (a > d && b > c)) {
-            res = 2;
-        }
+        int res = 2 * (((a > c) - (a < c)) + ((b > d) - (b < d)) > 0)
+                + 2 * (((a > d) - (a < d)) + ((b > c) - (b < c)) > 0);
 
         cout << res << endl;
     }
