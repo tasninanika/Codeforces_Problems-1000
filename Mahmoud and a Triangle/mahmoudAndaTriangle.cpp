5901f1badb9b27912a670f13ac4ad22914237147
anikatasnin <jarintasnin09@gmail.com>
2025-05-22 15:03:00 +0600

    added Mahmoud and a Triangle

--- Mahmoud and a Triangle/mahmoudAndaTriangle.cpp
+++ Mahmoud and a Triangle/mahmoudAndaTriangle.cpp
@@ -9,8 +9,7 @@ int main(){
         vector<int> v;
         int x;
 
-        for(int i = 0; i < n; i++)
-        {
+        for(int i = 0; i < n; i++){
             cin >> x;
             v.push_back(x);
         }
