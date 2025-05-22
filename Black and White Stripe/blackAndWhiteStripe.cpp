f90ec47ca12bb72cfb45371a3995fda9e9a8f4e5
anikatasnin <jarintasnin09@gmail.com>
2025-05-22 14:36:48 +0600

    added Black and White Stripe

--- Black and White Stripe/blackAndWhiteStripe.cpp
+++ Black and White Stripe/blackAndWhiteStripe.cpp
@@ -7,31 +7,29 @@ int main(){
 
     while(t--){
 
-    }
-    long long n, k;
-    cin >> n >> k;
+        long long n, k;
+        cin >> n >> k;
 
-    string s;
-    cin >> s;
+        string s;
+        cin >> s;
 
-    vector<long long> preSum(n + 1);
+        vector<long long> preSum(n + 1);
 
-    for (int i = 1; i <= n; i++){
-        long long curr = 0;
-        if (s[i - 1] == 'W'){
-            curr = 1;
+        for (int i = 1; i <= n; i++){
+            long long curr = 0;
+            if (s[i - 1] == 'W'){
+                curr = 1;
+            }
+            preSum[i] = preSum[i - 1] + curr;
         }
-        preSum[i] = preSum[i - 1] + curr;
+
+        long long res = INT_MAX;
+
+        for(int i = k; i <= n; i++){
+            long long curr = preSum[i] - preSum[i - k];
+            res = min(res, curr);
+        }
+        cout << res << endl;
     }
-
-    long long res = INT_MAX;
-
-    for(int i = k; i <= n; i++){
-        long long curr = preSum[i] - preSum[i - k];
-        res = min(res, curr);
-    }
-    cout << res << endl;
-
-
     return 0;
 }
