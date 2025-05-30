8582a390329a6a0bd46d556001c394d758bafe68
anikatasnin <jarintasnin09@gmail.com>
2025-05-30 11:09:37 +0600

    added Not Adjacent Matrix

--- Not Adjacent Matrix/notAdjacentMatrix.cpp
+++ Not Adjacent Matrix/notAdjacentMatrix.cpp
@@ -30,7 +30,9 @@ int main(){
                 l = (n * n) / 2 + 1;
             }
             for(int i = 0; i < n; i++){
+                for(int j = 0; j < n; j++){
 
+                }
             }
         }
     }
