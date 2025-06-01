cebc946c17b51c59de72563a87e9990823c9d772
anikatasnin <jarintasnin09@gmail.com>
2025-06-01 19:24:04 +0600

    added String LCM

--- String LCM/stringLCM.cpp
+++ String LCM/stringLCM.cpp
@@ -6,7 +6,30 @@ int main(){
     cin >> t;
 
     while(t--){
+        string a, b;
+        cin >> a >> b;
 
+        int lena = a.size();
+        int lenb = b.size();
+
+        int gcd = __gcd(lena, lenb);
+        int len = (lena * lenb) / gcd;
+
+
+        string sa = "", sb = "";
+        for(int i = 0; i < len / lena; i++){
+            sa += a;
+        }
+        for (int i = 0; i < len / lenb; i++){
+            sb += b;
+        }
+
+        if(sa == sb){
+            cout << sa << endl;
+        }
+        else{
+            cout << -1 << endl;
+        }
     }
     return 0;
 }
