8f7a8382141a77fa9910016e7c8e30709fd9db41
anikatasnin <jarintasnin09@gmail.com>
2025-06-01 19:18:38 +0600

    added String LCM

--- String LCM/stringLCM.cpp
+++ String LCM/stringLCM.cpp
@@ -16,7 +16,7 @@ int main(){
     for(int i = 0; i < len / lena; i++){
         sa += a;
     }
-    for (int i = 0; i < len / b.size(); i++){
+    for (int i = 0; i < len / lenb; i++){
         sb += b;
     }
 
