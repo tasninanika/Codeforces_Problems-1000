23c41d71df215f927891bd9f199064e2f0b06166
anikatasnin <jarintasnin09@gmail.com>
2025-06-07 19:48:30 +0600

    added Different Divisors

--- Different Divisors/differentDivisors.cpp
+++ Different Divisors/differentDivisors.cpp
@@ -1,41 +1,46 @@
-#include<iostream>
+#include <bits/stdc++.h>
 using namespace std;
 
 int main(){
     long long t;
     cin >> t;
 
-    vector<long long> a;
-    for(long long i = n + 1; ; i++){
-        int isPrime = 1;
+    while(t--){
+        long long n;
+        cin >> n;
 
-        for(long long j = 2; j * j <= i; j++){
-            if(i % j == 0){
-                isPrime = 0;
+        vector<long long> a;
+
+        for(long long i = n + 1; ; i++){
+            int isPrime = 1;
+            for(long long j = 2; j * j <= i; j++){
+                if(i % j == 0){
+                    isPrime = 0;
+                    break;
+                }
+            }
+            if(isPrime){
+                a.push_back(i);
                 break;
             }
         }
 
-        if(isPrime){
-            a.push_back(i);
-            break;
-        }
-    }
-
-    for(long long i = a.back() + n; ; i++){
-        int isPrime = 1;
-        for(long long j = 2; j * j <= i; j++){
-            if(i % j == 0){
-                isPrime = 0;
+        for(long long i = a.back() + n; ; i++){
+            int isPrime = 1;
+            for(long long j = 2; j * j <= i; j++){
+                if(i % j == 0){
+                    isPrime = 0;
+                    break;
+                }
+            }
+            if(isPrime){
+                a.push_back(i);
                 break;
             }
         }
-        if(isPrime){
-            a.push_back(i);
-            break;
-        }
-    }
 
+        cout << min(a[0] * a[1], a[0] * a[0] * a[0]) << endl;
+    }
 
     return 0;
 }
