bba4de75f6ba1c13e3cbd8d98954a4007c8391f0
anikatasnin <jarintasnin09@gmail.com>
2025-06-13 10:49:53 +0600

    added Monsters

--- Monsters/monsters.cpp
+++ Monsters/monsters.cpp
@@ -25,8 +25,8 @@ int main(){
 
         sort(h.begin(), h.end());
 
-        for(int i = 0; i < res.size(); i++){
-
+        for(int i = 0; i < h.size(); i++){
+            cout << h[i].second + 1 << " ";
         }
     }
 
