95501932a0801aff178e2738d58431008249afe9
anikatasnin <jarintasnin09@gmail.com>
2025-06-20 15:13:15 +0600

    added Payment Without Change

--- Payment Without Change/paymentWithoutChange.cpp
+++ Payment Without Change/paymentWithoutChange.cpp
@@ -1,1 +1,25 @@
+#include<bits/stdc++.h>
+using namespace std;
 
+int main(){
+    int t;
+    cin >> t;
+
+    long long int a, b, n, s, sum;
+
+    while(t--){
+        sum = 0;
+        cin >> a >> b >> n >> s;
+
+        long long int d = s / n;
+		d = min(d, a);
+
+		if(s - n * d <= b){
+			cout << "YES" << endl;
+		}
+		else{
+			cout << "NO" << endl;
+		}
+    }
+    return 0;
+}
