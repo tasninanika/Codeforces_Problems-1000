537bb75a8b1f7d89000e5d4d8451e0dfb6ae28f2
anikatasnin <jarintasnin09@gmail.com>
2025-07-02 11:17:50 +0600

    added Red and Blue

--- Red and Blue/redAndBlue.cpp
+++ Red and Blue/redAndBlue.cpp
@@ -1,1 +1,30 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+    int t;
+    cin >> t;
+
+    while(t--){
+        int r, b, d;
+        cin >> r >> b >> d;
+
+        if(r < b){
+            int temp = r;
+            r = b;
+            b = temp;
+        }
+
+        int max_needed = (r + b - 1) / b - 1;
+
+        if(max_needed <= d){
+            cout << "YES" << endl;
+        }
+        else{
+            cout << "NO" << endl;
+        }
+    }
+
+    return 0;
+}
 
