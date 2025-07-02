a89c9a05c65052ded35cea7572a7f17569d9eca2
anikatasnin <jarintasnin09@gmail.com>
2025-07-02 11:13:29 +0600

    added Choose the Different Ones!

--- Choose the Different Ones!/chooseTheDifferentOnes!.cpp
+++ Choose the Different Ones!/chooseTheDifferentOnes!.cpp
@@ -1,29 +1,31 @@
-#include <iostream>
+#include<iostream>
 using namespace std;
 
-int main() {
+int main(){
     int t;
     cin >> t;
 
-    while (t--) {
+    while(t--){
         int n, m, k;
         cin >> n >> m >> k;
 
         vector<int> a(k + 1, 0);
         vector<int> b(k + 1, 0);
 
-        for (int i = 0; i < n; i++) {
+        for(int i = 0; i < n; i++){
             int x;
             cin >> x;
-            if (x <= k) {
+
+            if(x <= k){
                 a[x] = 1;
             }
         }
 
-        for (int i = 0; i < m; i++) {
+        for(int i = 0; i < m; i++){
             int x;
             cin >> x;
-            if (x <= k) {
+
+            if(x <= k){
                 b[x] = 1;
             }
         }
@@ -31,26 +33,26 @@ int main() {
         int onlyA = 0, onlyB = 0, both = 0;
         int possible = 1;
 
-        for (int i = 1; i <= k; i++) {
-            if (a[i] == 1 && b[i] == 1) {
+        for(int i = 1; i <= k; i++){
+            if(a[i] == 1 && b[i] == 1){
                 both++;
             }
-            else if (a[i] == 1) {
+            else if(a[i] == 1){
                 onlyA++;
             }
-            else if (b[i] == 1) {
+            else if(b[i] == 1){
                 onlyB++;
             }
-            else {
+            else{
                 possible = 0;
                 break;
             }
         }
 
-        if (possible == 1 && (onlyA + both >= k / 2) && (onlyB + both >= k / 2)) {
+        if(possible == 1 && (onlyA + both >= k / 2) && (onlyB + both >= k / 2)){
             cout << "YES" << endl;
         }
-        else {
+        else{
             cout << "NO" << endl;
         }
     }
