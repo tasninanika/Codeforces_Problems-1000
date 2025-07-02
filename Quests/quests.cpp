ca1c2a794c3a648000bbd1e9380c803fb902a498
anikatasnin <jarintasnin09@gmail.com>
2025-07-02 10:48:03 +0600

    added Quests

--- Quests/quests.cpp
+++ Quests/quests.cpp
@@ -1,1 +1,46 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+    int t;
+    cin >> t;
+
+    while(t--){
+        int n, k;
+        cin >> n >> k;
+
+        vector<int> a(n);
+        vector<int> b(n);
+
+        for(int i = 0; i < n; i++){
+            cin >> a[i];
+        }
+
+        for(int i = 0; i < n; i++){
+            cin >> b[i];
+        }
+
+        int sumA = 0;
+        int maxB = 0;
+        int ans = 0;
+
+        for(int i = 0; i < n && i < k; i++){
+            sumA += a[i];
+
+            if(b[i] > maxB){
+                maxB = b[i];
+            }
+
+            int current = sumA + (k - i - 1) * maxB;
+
+            if(current > ans){
+                ans = current;
+            }
+        }
+
+        cout << ans << endl;
+    }
+
+    return 0;
+}
 
