68803ef7beb44754f2ea19d89b7e115ca9f20723
anikatasnin <jarintasnin09@gmail.com>
2025-07-06 22:58:48 +0600

    added Double-ended Strings

--- Double-ended Strings/double-endedStrings.cpp
+++ Double-ended Strings/double-endedStrings.cpp
@@ -1,36 +1,36 @@
 #include<bits/stdc++.h>
 using namespace std;
 
-int main(){
+int main() {
     int t;
     cin >> t;
 
-    while(t--){
+    while (t--) {
         string s1, s2;
         cin >> s1 >> s2;
 
-        int n = s1.length();
-        int m = s2.length();
+        int n = s1.size();
+        int m = s2.size();
 
-        int dp[1010][1010];
-        memset(dp, 0, sizeof(dp));
+        int dp[101][101] = {0};
 
-        int res = 0;
+        int maxLength = 0;
 
-        for (int i = 1; i <= n; i++){
-            for (int j = 1; j <= m; j++){
-                if (s1[i - 1] == s2[j - 1]){
-                    dp[i][j] = 1 + dp[i - 1][j - 1];
+        for (int i = 1; i <= n; i++) {
+            for (int j = 1; j <= m; j++) {
+                if (s1[i - 1] == s2[j - 1]) {
+                    dp[i][j] = dp[i - 1][j - 1] + 1;
+                    maxLength = max(maxLength, dp[i][j]);
                 }
-                else{
+                else {
                     dp[i][j] = 0;
                 }
-
-                res = max(res, dp[i][j]);
             }
         }
 
-        cout << (n + m - 2 * res) << endl;
+        int answer = n + m - 2 * maxLength;
+
+        cout << answer << endl;
     }
 
     return 0;
