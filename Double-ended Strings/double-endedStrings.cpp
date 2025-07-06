66c6ac50017bd78e717af4187b44d0cda5c9f008
anikatasnin <jarintasnin09@gmail.com>
2025-07-06 22:55:16 +0600

    added Double-ended Strings

--- Double-ended Strings/double-endedStrings.cpp
+++ Double-ended Strings/double-endedStrings.cpp
@@ -1,1 +1,37 @@
+#include<bits/stdc++.h>
+using namespace std;
 
+int main(){
+    int t;
+    cin >> t;
+
+    while(t--){
+        string s1, s2;
+        cin >> s1 >> s2;
+
+        int n = s1.length();
+        int m = s2.length();
+
+        int dp[1010][1010];
+        memset(dp, 0, sizeof(dp));
+
+        int res = 0;
+
+        for (int i = 1; i <= n; i++){
+            for (int j = 1; j <= m; j++){
+                if (s1[i - 1] == s2[j - 1]){
+                    dp[i][j] = 1 + dp[i - 1][j - 1];
+                }
+                else{
+                    dp[i][j] = 0;
+                }
+
+                res = max(res, dp[i][j]);
+            }
+        }
+
+        cout << (n + m - 2 * res) << endl;
+    }
+
+    return 0;
+}
