85c705e8f5e3c3fee87c6eee7578f1d832ad47cd
anikatasnin <jarintasnin09@gmail.com>
2025-07-08 23:48:33 +0600

    added Numeric String Template

--- Numeric String Template/numericStringTemplate.cpp
+++ Numeric String Template/numericStringTemplate.cpp
@@ -1,1 +1,56 @@
+#include <bits/stdc++.h>
+using namespace std;
 
+int main() {
+    int t;
+    cin >> t;
+
+    while (t--) {
+        int n;
+        cin >> n;
+
+        int a[100];
+        for (int i = 0; i < n; i++) {
+            cin >> a[i];
+        }
+
+        int m;
+        cin >> m;
+
+        while (m--) {
+            string s;
+            cin >> s;
+
+            if (s.length() != n) {
+                cout << "NO" << endl;
+                continue;
+            }
+
+            map<int, char> num_to_char;
+            map<char, int> char_to_num;
+            bool isValid = true;
+
+            for (int i = 0; i < n; i++) {
+                int number = a[i];
+                char letter = s[i];
+
+                if ((num_to_char.count(number) && num_to_char[number] != letter) ||
+                    (char_to_num.count(letter) && char_to_num[letter] != number)) {
+                    isValid = false;
+                    break;
+                }
+
+                num_to_char[number] = letter;
+                char_to_num[letter] = number;
+            }
+
+            if (isValid) {
+                cout << "YES" << endl;
+            } else {
+                cout << "NO" << endl;
+            }
+        }
+    }
+
+    return 0;
+}
