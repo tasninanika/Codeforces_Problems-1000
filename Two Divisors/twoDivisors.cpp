6e155ef356ebf2767b437ea06765b416493754f8
anikatasnin <jarintasnin09@gmail.com>
2025-07-09 23:27:39 +0600

    added Two Divisors

--- Two Divisors/twoDivisors.cpp
+++ Two Divisors/twoDivisors.cpp
@@ -1,1 +1,31 @@
+#include<iostream>
+using namespace std;
+
+long gcd(long a, long b){
+    if (b == 0) return a;
+    return gcd(b, a % b);
+}
+
+int main(){
+    int t;
+    cin >> t;
+
+    while(t--){
+        long a, b;
+        cin >> a >> b;
+
+        long g = gcd(a, b);
+
+        long result;
+        if(b % a != 0){
+            result = (a / g) * b;
+        }
+        else{
+            result = b;
+        }
+
+        cout << result << endl;
+    }
+
+
 
