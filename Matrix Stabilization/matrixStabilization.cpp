2e24c2380b6a5b128f27476993ad5d514870904c
anikatasnin <jarintasnin09@gmail.com>
2025-07-10 23:51:48 +0600

    added Matrix Stabilization

--- Matrix Stabilization/matrixStabilization.cpp
+++ Matrix Stabilization/matrixStabilization.cpp
@@ -11,11 +11,13 @@ int main(){
 
         vector<vector<int>> a(n + 2, vector<int>(m + 2, 0));
 
-        for (int i = 1; i <= n; ++i)
-            for (int j = 1; j <= m; ++j)
+        for(int i = 1; i <= n; ++i){
+            for(int j = 1; j <= m; ++j){
                 cin >> a[i][j];
+            }
+        }
 
-        for (int i = 1; i <= n; ++i) {
+        for(int i = 1; i <= n; ++i){
             for (int j = 1; j <= m; ++j) {
                 int maxAround = max({a[i - 1][j], a[i + 1][j], a[i][j - 1], a[i][j + 1]});
                 cout << min(a[i][j], maxAround) << " ";
