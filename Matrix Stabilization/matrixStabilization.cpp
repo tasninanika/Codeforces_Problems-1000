82e845746b0aa952b479578cd01e73a728373f18
anikatasnin <jarintasnin09@gmail.com>
2025-07-10 23:46:51 +0600

    added Matrix Stabilization

--- Matrix Stabilization/matrixStabilization.cpp
+++ Matrix Stabilization/matrixStabilization.cpp
@@ -3,6 +3,33 @@ using namespace std;
 
 int main(){
     int t;
+    cin >> t;
+
+    while(t--){
+        int n, m;
+        cin >> n >> m;
+
+        int a[102][102];
+
+        for (int i = 1; i <= n; i++) {
+            for (int j = 1; j <= m; j++) {
+                cin >> a[i][j];
+            }
+        }
+
+        for (int i = 1; i <= n; i++) {
+            for (int j = 1; j <= m; j++) {
+                int up = a[i - 1][j];
+                int down = a[i + 1][j];
+                int left = a[i][j - 1];
+                int right = a[i][j + 1];
+                int maxNeighbour = max(max(up, down), max(left, right));
+                int result = min(a[i][j], maxNeighbour);
+                cout << result << " ";
+            }
+            cout << endl;
+        }
+    }
 
     return 0;
 }
