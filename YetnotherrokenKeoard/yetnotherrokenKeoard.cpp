e8737a748427c481a57d4ad6e107b63e8f009cef
anikatasnin <jarintasnin09@gmail.com>
2025-07-25 14:42:25 +0600

    added YetnotherrokenKeoard

--- YetnotherrokenKeoard/yetnotherrokenKeoard.cpp
+++ YetnotherrokenKeoard/yetnotherrokenKeoard.cpp
@@ -1,52 +1,50 @@
 #include<bits/stdc++.h>
 using namespace std;
 
-int main(){
+int main() {
     int t;
     cin >> t;
-
-    while(t--){
+    while (t--) {
         string str;
         cin >> str;
 
-        string lower = " ", upper = " ";
+        vector<pair<char, int>> lower, upper;
 
-        for(int i = 0; i < str.size(); i++){
+        for(int i = 0; i < (int)str.size(); i++){
             char c = str[i];
 
-            if(islower(c)){
-                if(c == 'b' && !lower.empty()){
+            if (islower(c)) {
+                if (c == 'b' && !lower.empty()) {
                     lower.pop_back();
                 }
-                else if(c != 'b'){
-                    lower.push_back(c);
+                else if (c != 'b') {
+                    lower.push_back({c, i});
                 }
             }
-            else{
-                if(c == 'B' && !upper.empty()){
+            else {
+                if (c == 'B' && !upper.empty()) {
                     upper.pop_back();
                 }
-                else if(c != 'B'){
-                    upper.push_back(c);
+                else if (c != 'B') {
+                    upper.push_back({c, i});
                 }
             }
         }
 
-        string ans = "";
-        int i = 0, j = 0;
-        for(int k = 0; k < (int)str.size(); k++){
-            if(i < (int)lower.size() && str[k] == lower[i]){
-                ans.push_back(lower[i]);
-                i++;
-            }
-            else if(j < (int)upper.size() && str[k] == upper[j]){
-                ans.push_back(upper[j]);
-                j++;
-            }
+        vector<pair<char, int>> ans;
+        for (int i = 0; i < (int)lower.size(); i++)
+            ans.push_back(lower[i]);
+        for (int i = 0; i < (int)upper.size(); i++)
+            ans.push_back(upper[i]);
+
+        sort(ans.begin(), ans.end(), [](const pair<char, int>& a, const pair<char, int>& b) {
+            return a.second < b.second;
+        });
+
+        for (int i = 0; i < (int)ans.size(); i++) {
+            cout << ans[i].first;
         }
-        cout << ans << endl;
+        cout << endl;
     }
-
-
     return 0;
 }
