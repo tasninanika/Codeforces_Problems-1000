68a0537fe7353bd452b7a0a108220ccb55a5cd70
anikatasnin <jarintasnin09@gmail.com>
2025-07-25 15:19:17 +0600

    added Letters

--- Letters/letters.cpp
+++ Letters/letters.cpp
@@ -1,1 +1,26 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+    long long n, m;
+    cin >> n >> m;
+
+    long long a[n + 1];
+    for(long long i = 1; i <= n; i++){
+        cin >> a[i];
+    }
+
+    long long t = 1, sum = 0;
+    for(long long i = 0; i < m; i++){
+        long long x;
+        cin >> x;
+
+        while(sum + a[t] < x){
+            sum += a[t];
+            t++;
+        }
+        cout << t << " " << (x - sum) << endl;
+    }
+    return 0;
+}
 
