c7d2e23a9da8bdcd1dcb523d5e6cff8b033d4d25
anikatasnin <jarintasnin09@gmail.com>
2025-07-25 15:24:41 +0600

    added Maximum Cost Deletion

--- Maximum Cost Deletion/maximumCostDeletion.cpp
+++ Maximum Cost Deletion/maximumCostDeletion.cpp
@@ -1,1 +1,33 @@
+#include <iostream>
+using namespace std;
 
+int main() {
+    int t;
+    cin >> t;
+
+    while(t--){
+        int n, a, b;
+        cin >> n >> a >> b;
+
+        string str;
+        cin >> str;
+
+        int c = 0;
+        for(int i = 0; i < n - 1; i++){
+            if(str[i] != str[i + 1]){
+                c++;
+            }
+        }
+
+        int result;
+        if(b >= 0){
+            result = a * n + b * n;
+        }
+        else{
+            result = a * n + b * (c / 2 + 1);
+        }
+
+        cout << result << endl;
+    }
+    return 0;
+}
