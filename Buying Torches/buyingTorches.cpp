fe8eed2007c16b6d0514483c410b1b5e50e598a4
anikatasnin <jarintasnin09@gmail.com>
2025-07-26 15:00:24 +0600

    added Buying Torches

--- Buying Torches/buyingTorches.cpp
+++ Buying Torches/buyingTorches.cpp
@@ -1,1 +1,22 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main(){
+    int t;
+    cin >> t;
+
+    while(t--){
+        long long x, y, k;
+        cin >> x >> y >> k;
+
+        long long coal = y * k;
+        long long sticks = coal + (k - 1);
+
+        long long div = (sticks + (x - 2)) / (x - 1);
+        long long ans = div + k;
+
+        cout << ans << endl;
+    }
+    return 0;
+}
 
