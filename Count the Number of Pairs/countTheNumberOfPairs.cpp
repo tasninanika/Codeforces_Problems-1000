e1df7877f9a1b1c6150fa67c60c1edb8dbbe8543
anikatasnin <jarintasnin09@gmail.com>
2025-07-26 15:13:16 +0600

    added Count the Number of Pairs

--- Count the Number of Pairs/countTheNumberOfPairs.cpp
+++ Count the Number of Pairs/countTheNumberOfPairs.cpp
@@ -1,1 +1,58 @@
+#include <iostream>
+#include <string>
+using namespace std;
 
+int main() {
+    int t;
+    cin >> t;
+    while (t--) {
+        int n, k;
+        cin >> n >> k;
+        string s;
+        cin >> s;
+
+        int small[26] = {0};
+        int big[26] = {0};
+
+        for (int i = 0; i < n; i++) {
+            if (s[i] >= 'A' && s[i] <= 'Z') {
+                big[s[i] - 'A']++;
+            } else {
+                small[s[i] - 'a']++;
+            }
+        }
+
+        int ans = 0;
+        for (int i = 0; i < 26; i++) {
+            int pairs;
+            if (small[i] < big[i]) {
+                pairs = small[i];
+            } else {
+                pairs = big[i];
+            }
+
+            ans += pairs;
+            small[i] -= pairs;
+            big[i] -= pairs;
+
+            int remaining;
+            if (small[i] > big[i]) {
+                remaining = small[i];
+            } else {
+                remaining = big[i];
+            }
+
+            int add = remaining / 2;
+            if (add > k) {
+                add = k;
+            }
+
+            ans += add;
+            k -= add;
+        }
+
+        cout << ans << "\n";
+    }
+
+    return 0;
+}
