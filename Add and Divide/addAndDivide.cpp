0df513a67cc1dc16e42bd5847aae0d681f4358ce
anikatasnin <jarintasnin09@gmail.com>
2025-08-08 12:09:58 +0600

    added Add and Divide

--- Add and Divide/addAndDivide.cpp
+++ Add and Divide/addAndDivide.cpp
@@ -1,27 +1,27 @@
-#include <iostream>
+#include<iostream>
 using namespace std;
 
-int main() {
+int main(){
     ios::sync_with_stdio(false);
     cin.tie(nullptr);
 
     int t;
     cin >> t;
 
-    while (t--) {
+    while(t--){
         int n, m;
         cin >> n >> m;
 
         int result = 1e9;
 
-        for (int add = 0; add <= 30; ++add) {
+        for(int add = 0; add <= 30; ++add){
             int base = m + add;
-            if (base == 1)
+            if(base == 1)
                 continue;
 
             int x = n;
             int ops = 0;
-            while (x > 0) {
+            while(x > 0){
                 x /= base;
                 ops++;
             }
