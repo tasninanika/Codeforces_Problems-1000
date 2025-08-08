05fa9c2a761583cd3fbb0215f5789e52ab04d91e
anikatasnin <jarintasnin09@gmail.com>
2025-08-08 12:17:20 +0600

    added Add and Divide

--- Simple Repetition/simpleRepetition.cpp
+++ Simple Repetition/simpleRepetition.cpp
@@ -1,26 +1,28 @@
 #include<iostream>
 using namespace std;
 
-int main() {
+int main(){
     ios::sync_with_stdio(false);
     cin.tie(nullptr);
 
     int t;
     cin >> t;
 
-    while (t--) {
+    while(t--){
         long long n;
         int k;
         cin >> n >> k;
 
         bool isPrime = true;
 
-        if (k == 1) {
+        if(k == 1){
             if (n < 2) isPrime = false;
-            else if (n == 2) isPrime = true;
-            else if (n % 2 == 0) isPrime = false;
-            else {
-                for (long long i = 3; i * i <= n; i += 2) {
+            else if(n == 2)
+             isPrime = true;
+            else if(n % 2 == 0)
+             isPrime = false;
+            else{
+                for(long long i = 3; i * i <= n; i += 2){
                     if (n % i == 0) {
                         isPrime = false;
                         break;
@@ -28,21 +30,24 @@ int main() {
                 }
             }
         }
-        else if (n != 1) {
+        else if(n != 1){
             isPrime = false;
         }
-        else {
+        else{
             long long number = 0;
-            for (int i = 0; i < k; ++i) {
+            for(int i = 0; i < k; ++i){
                 number = number * 10 + 1;
             }
 
-            if (number < 2) isPrime = false;
-            else if (number == 2) isPrime = true;
-            else if (number % 2 == 0) isPrime = false;
-            else {
-                for (long long i = 3; i * i <= number; i += 2) {
-                    if (number % i == 0) {
+            if(number < 2)
+                isPrime = false;
+            else if(number == 2)
+             isPrime = true;
+            else if(number % 2 == 0)
+             isPrime = false;
+            else{
+                for(long long i = 3; i * i <= number; i += 2){
+                    if(number % i == 0){
                         isPrime = false;
                         break;
                     }
