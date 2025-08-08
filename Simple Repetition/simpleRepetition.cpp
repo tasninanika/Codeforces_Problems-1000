34379d491905c3eb42eaeb78972b8188261e00d2
anikatasnin <jarintasnin09@gmail.com>
2025-08-08 12:15:32 +0600

    added Simple Repetition

--- Simple Repetition/simpleRepetition.cpp
+++ Simple Repetition/simpleRepetition.cpp
@@ -1,1 +1,57 @@
+#include<iostream>
+using namespace std;
 
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+    int t;
+    cin >> t;
+
+    while (t--) {
+        long long n;
+        int k;
+        cin >> n >> k;
+
+        bool isPrime = true;
+
+        if (k == 1) {
+            if (n < 2) isPrime = false;
+            else if (n == 2) isPrime = true;
+            else if (n % 2 == 0) isPrime = false;
+            else {
+                for (long long i = 3; i * i <= n; i += 2) {
+                    if (n % i == 0) {
+                        isPrime = false;
+                        break;
+                    }
+                }
+            }
+        }
+        else if (n != 1) {
+            isPrime = false;
+        }
+        else {
+            long long number = 0;
+            for (int i = 0; i < k; ++i) {
+                number = number * 10 + 1;
+            }
+
+            if (number < 2) isPrime = false;
+            else if (number == 2) isPrime = true;
+            else if (number % 2 == 0) isPrime = false;
+            else {
+                for (long long i = 3; i * i <= number; i += 2) {
+                    if (number % i == 0) {
+                        isPrime = false;
+                        break;
+                    }
+                }
+            }
+        }
+
+        cout << (isPrime ? "YES" : "NO") << '\n';
+    }
+
+    return 0;
+}
