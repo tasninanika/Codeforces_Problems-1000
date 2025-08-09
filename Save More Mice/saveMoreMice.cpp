74f807b7f3f390d4c7569718a5584139a9007b7e
anikatasnin <jarintasnin09@gmail.com>
2025-08-09 23:17:01 +0600

    added Save More Mice

--- Save More Mice/saveMoreMice.cpp
+++ Save More Mice/saveMoreMice.cpp
@@ -1,1 +1,30 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+    int t;
+    cin >> t;
+    while (t--) {
+        int n, k;
+        cin >> n >> k;
+        vector<int> v(k);
+        for (int i = 0; i < k; i++) {
+            cin >> v[i];
+        }
+        sort(v.begin(), v.end());
+
+        int cnt = 0, cat = 0;
+        while (cat < v.back()) {
+            cat += (n - v.back());
+            cnt++;
+            v.pop_back();
+            if (v.empty()) break;
+        }
+        cout << cnt << "\n";
+    }
+    return 0;
+}
 
