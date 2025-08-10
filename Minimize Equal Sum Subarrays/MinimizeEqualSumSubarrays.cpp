6f45dd9c545e45155e720a68791d2bff26a09778
anikatasnin <jarintasnin09@gmail.com>
2025-08-10 23:44:15 +0600

    added Minimize Equal Sum Subarrays

--- Minimize Equal Sum Subarrays/MinimizeEqualSumSubarrays.cpp
+++ Minimize Equal Sum Subarrays/MinimizeEqualSumSubarrays.cpp
@@ -1,1 +1,31 @@
+#include<bits/stdc++.h>
+using namespace std;
+#define ll long long
+
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+
+    ll t;
+    cin >> t;
+
+    while (t--) {
+        ll n;
+        cin >> n;
+
+        ll arr[n];
+        for (int i = 0; i < n; i++) {
+            cin >> arr[i];
+        }
+
+        rotate(arr, arr + 1, arr + n);
+
+        for (int i = 0; i < n; i++) {
+            cout << arr[i] << " ";
+        }
+        cout << endl;
+    }
+
+    return 0;
+}
 
