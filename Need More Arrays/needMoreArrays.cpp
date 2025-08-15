d83c7f74b3c84e4c8f2cb93197641283f58f41d7
anikatasnin <jarintasnin09@gmail.com>
2025-08-15 21:46:17 +0600

    added Need More Arrays

--- Need More Arrays/needMoreArrays.cpp
+++ Need More Arrays/needMoreArrays.cpp
@@ -1,1 +1,26 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+    int t;
+    cin >> t;
+
+    while(t--){
+        int n;
+        cin >> n;
+
+        vector<int> a(n);
+        for(int i = 0; i < n; i++)
+            cin >> a[i];
+
+        set<int> s(a.begin(), a.end());
+
+        cout << s.size() << endl;
+    }
+
+    return 0;
+}
 
