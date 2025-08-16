f6ae70f04b088572b9f2753c0641b3911541a10f
anikatasnin <jarintasnin09@gmail.com>
2025-08-17 00:39:35 +0600

    added Farmer John's Card Game

--- Farmer John's Card Game/farmerJohn'sCardGame.cpp
+++ Farmer John's Card Game/farmerJohn'sCardGame.cpp
@@ -1,13 +1,14 @@
-#include <bits/stdc++.h>
+#include<bits/stdc++.h>
 using namespace std;
 
-int main() {
+int main(){
     ios::sync_with_stdio(false);
     cin.tie(nullptr);
 
     int t;
     cin >> t;
-    while (t--) {
+
+    while(t--){
         int n, m;
         cin >> n >> m;
 
