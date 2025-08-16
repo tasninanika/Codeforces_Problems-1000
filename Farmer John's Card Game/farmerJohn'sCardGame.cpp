39ae7314288a1c2ee474c3ce2adcb2125a2ee3a2
anikatasnin <jarintasnin09@gmail.com>
2025-08-17 00:38:06 +0600

    added Farmer John's Card Game

--- Farmer John's Card Game/farmerJohn'sCardGame.cpp
+++ Farmer John's Card Game/farmerJohn'sCardGame.cpp
@@ -1,1 +1,36 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+    int t;
+    cin >> t;
+    while (t--) {
+        int n, m;
+        cin >> n >> m;
+        vector<vector<int>> cards(n, vector<int>(m));
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < m; j++) {
+                cin >> cards[i][j];
+            }
+            sort(cards[i].begin(), cards[i].end());
+        }
+
+        vector<pair<int,int>> cows;
+        for (int i = 0; i < n; i++) {
+            cows.push_back({cards[i][0], i+1});
+        }
+
+        sort(cows.begin(), cows.end());
+
+        for (int i = 0; i < n; i++) {
+            cout << cows[i].second << " ";
+        }
+        cout << "\n";
+    }
+
+    return 0;
+}
 
