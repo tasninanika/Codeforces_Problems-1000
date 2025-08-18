7073117df0ccdba17c5c7e9517cd3c0c4dc357f0
anikatasnin <jarintasnin09@gmail.com>
2025-08-18 23:39:03 +0600

    added Dima and Friends

--- Dima and Friends/dimaAndFriends.cpp
+++ Dima and Friends/dimaAndFriends.cpp
@@ -1,1 +1,23 @@
+#include<bits/stdc++.h>
+using namespace std;
 
+int main(){
+    int n,m,i,sum;
+
+    while(cin >> n){
+        sum = 0;
+        for(i=0;i < n; i++){
+            cin >> m;
+            sum += m;
+        }
+        int f = n+1, c = 0;
+
+        for(i = sum + 1; i <= sum + 5; i++){
+            if(i % f ==1 ){
+                c++;
+            }
+        }
+        cout << 5 - c << endl;
+    }
+    return 0;
+}
