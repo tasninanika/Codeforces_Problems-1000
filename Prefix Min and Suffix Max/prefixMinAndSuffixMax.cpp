9906fd0b345c1c1786caeed91293e2e7a5124206
anikatasnin <jarintasnin09@gmail.com>
2025-08-23 06:58:38 +0600

    added Prefix Min and Suffix Max

--- Prefix Min and Suffix Max/prefixMinAndSuffixMax.cpp
+++ Prefix Min and Suffix Max/prefixMinAndSuffixMax.cpp
@@ -1,1 +1,40 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(0);
+
+    int t;
+    cin >> t;
+
+    while (t--) {
+        int n;
+        cin >> n;
+
+        vector<long long> v(n);
+        for (int i = 0; i < n; i++)
+            cin >> v[i];
+
+        vector<long long> mn(n), mx(n);
+
+        mn[0] = v[0];
+        for (int i = 1; i < n; i++)
+            mn[i] = min(mn[i - 1], v[i]);
+
+        mx[n - 1] = v[n - 1];
+        for (int i = n - 2; i >= 0; i--)
+            mx[i] = max(mx[i + 1], v[i]);
+
+        for (int i = 0; i < n; i++) {
+            if (v[i] == mn[i] || v[i] == mx[i])
+                cout << 1;
+            else
+                cout << 0;
+        }
+        cout << endl;
+    }
+
+    return 0;
+}
 
