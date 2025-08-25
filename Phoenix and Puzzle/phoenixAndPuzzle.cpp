0b245f0c376f71e4a1e92314e0a8a8fa5343b935
anikatasnin <jarintasnin09@gmail.com>
2025-08-25 18:13:34 +0600

    added Phoenix and Puzzle

--- Phoenix and Puzzle/phoenixAndPuzzle.cpp
+++ Phoenix and Puzzle/phoenixAndPuzzle.cpp
@@ -26,6 +26,9 @@ int main(){
         }
         if(!found && n % 4 == 0){
             long long x = sqrt(n / 4);
+            for(long long i = x - 10; i <= x + 10; i++){
+
+            }
         }
     }
 
