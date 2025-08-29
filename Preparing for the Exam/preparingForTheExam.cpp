b4ee14bb0cbb415a0d45902e3c1c35fd666f055a
anikatasnin <jarintasnin09@gmail.com>
2025-08-29 23:15:18 +0600

    added Preparing for the Exam

--- Preparing for the Exam/preparingForTheExam.cpp
+++ Preparing for the Exam/preparingForTheExam.cpp
@@ -17,18 +17,18 @@ int main() {
         vector<bool> known_questions(n + 1, false);
 
         vector<int> a(m);
-        for (int i = 0; i < m; ++i) {
+        for (int i = 0; i < m; i++) {
             cin >> a[i];
         }
 
-        for (int i = 0; i < k; ++i) {
+        for(int i = 0; i < k; i++){
             int q;
             cin >> q;
             known_questions[q] = true;
         }
 
         string result = "";
-        for(int i = 0; i < m; ++i) {
+        for(int i = 0; i < m; i++) {
             int excluded_question = a[i];
 
             if(known_questions[excluded_question] == false) {
@@ -38,7 +38,7 @@ int main() {
                 result += '0';
             }
         }
-        cout << result << '\n';
+        cout << result << endl;
     }
 
     return 0;
