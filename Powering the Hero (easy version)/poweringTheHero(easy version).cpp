86c78f158d42924149c3b897fe5b0c2dc8092a95
anikatasnin <jarintasnin09@gmail.com>
2025-08-31 18:39:22 +0600

    added Powering the Hero (easy version)

--- Powering the Hero (easy version)/poweringTheHero(easy version).cpp
+++ Powering the Hero (easy version)/poweringTheHero(easy version).cpp
@@ -1,1 +1,35 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+    int t;
+    cin >> t;
+
+    while (t--) {
+        int n;
+        cin >> n;
+        vector<long long> bonus;
+        int heroes = 0;
+
+        for (int i = 0; i < n; i++) {
+            long long x;
+            cin >> x;
+            if (x == 0) heroes++;
+            else bonus.push_back(x);
+        }
+
+        sort(bonus.rbegin(), bonus.rend());
+
+        long long ans = 0;
+        for (int i = 0; i < heroes && i < (int)bonus.size(); i++) {
+            ans += bonus[i];
+        }
+
+        cout << ans << "\n";
+    }
+    return 0;
+}
 
