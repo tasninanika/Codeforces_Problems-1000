5a31a88fa908989ca48c3370feb0f6782a171b76
anikatasnin <jarintasnin09@gmail.com>
2025-09-09 18:41:00 +0600

    added Pasha and Stick

--- Pasha and Stick/pashaAndStick.cpp
+++ Pasha and Stick/pashaAndStick.cpp
@@ -1,1 +1,25 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    long long n;
+
+    if (!(cin >> n))
+        return 0;
+
+    if (n % 2 == 1) {
+        cout << 0 << '\n';
+        return 0;
+    }
+
+    long long S = n / 2;
+    long long ans = (S - 1) / 2;
+
+    if (ans < 0)
+        ans = 0;
+
+    cout << ans << endl;
+
+    return 0;
+}
 
