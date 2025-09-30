924c8b3acd6335da0032f4c0bc417cf819ad054b
anikatasnin <jarintasnin09@gmail.com>
2025-09-30 11:47:29 +0600

    added Red Versus Blue

--- Little Artem/littleArtem.cpp
+++ Little Artem/littleArtem.cpp
@@ -2,9 +2,32 @@
 using namespace std;
 
 int main(){
-    int n, m;
-    cin >> n >> m;
+    int t;
+    cin >> t;
 
+    while(t--){
+        int n, m;
+        cin >> n >> m;
+
+        vector<vector<char>> grid(n, vector<char>(m));
+        for (int i = 0; i < n; ++i) {
+            for (int j = 0; j < m; ++j) {
+                if (i == 0 && j == 0) {
+                    grid[i][j] = 'W';
+                }
+                else {
+                    grid[i][j] = 'B';
+                }
+            }
+        }
+
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < m; j++) {
+                cout << grid[i][j];
+            }
+            cout << endl;
+        }
+    }
 
 
     return 0;
